f93ccd3dd3a2c38e0b8e5814e47a937d992ebe52
vy3191 <venkyafhs@gmail.com>
2020-04-15 17:46:20 -0400

    Boilerpate test setup done

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter});
+
+test('renders <App /> without errors', () => {
+    
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking button increments counter display', () => {
+
 });
